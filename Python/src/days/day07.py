c9c8c11518ad6ed371ebb79a2cd0f5a47a83228d
Vera van den Heuvel <ray10k@xmsnet.nl>
2023-12-07 11:32:32 +0100

    Some partial solutions

--- Python/src/days/day07.py
+++ Python/src/days/day07.py
@@ -1,16 +1,72 @@
 from pathlib import Path
 from time import perf_counter_ns
+from typing import NamedTuple
+from collections import Counter
 
-INPUT_NAME = "day07.txt"
+INPUT_NAME = "day07_1.txt"
 INPUT_PATH = Path(__file__).parent.parent.parent / "input" / INPUT_NAME
+CARD_ORDER = {rank:value for value,rank in enumerate("23456789TJQKA")}
 
-def parse_line(line:str):
+def hand_rank(hand:str) -> int:
+    counts = Counter(hand)
+    matches = sorted((x for x in counts.values()),reverse=True)
+    print(f"{hand} -> {matches}")
+    if matches == [5]:
+        return 6
+    if matches == [4,1]:
+        return 5
+    if matches == [3,2]:
+        return 4
+    if matches == [3,1,1]:
+        return 3
+    if matches == [2,2,1]:
+        return 2
+    if matches == [2,1,1,1]:
+        return 1
+    return 0
+
+class CardHand(NamedTuple):
+    cards:str
+    bid:int
+    rank:int
+    
+    def __gt__(self, __value: "CardHand") -> bool:
+        if self.rank != __value.rank:
+            return self.rank > __value.rank
+        for s,o in zip(self.cards,__value.cards):
+            if s == o:
+                continue
+            return CARD_ORDER[s] > CARD_ORDER[o]
+        return False
+    
+    def __ge__(self, __value: "CardHand") -> bool:
+        if self.rank != __value.rank:
+            return self.rank > __value.rank
+        for s,o in zip(self.cards,__value.cards):
+            if s == o:
+                continue
+            return CARD_ORDER[s] > CARD_ORDER[o]
+        return True
+    
+    def __eq__(self, __value: "CardHand") -> bool:
+        return self.cards == __value.cards
+    
+    def __repr__(self) -> str:
+        return f"{self.cards},{self.bid}. Rank {self.rank}"
+    
+    @classmethod
+    def from_line(cls,line:str):
+        parts = line.split(" ")
+        rank = hand_rank(parts[0])
+        return CardHand(parts[0],int(parts[1]),rank)
+        
+def parse_line(line:str) -> CardHand|None:
     """ Parse one line of the input into an 'object' for the solution.
     If a line should be discarded, return None instead. """
     line = line.strip()
     if line == "":
         return None
-    return line
+    return CardHand.from_line(line)
 
 def parse_input(file_path = INPUT_PATH):
     """ Loads the given file, and parses it line-by-line. Should return
@@ -21,10 +77,13 @@ def parse_input(file_path = INPUT_PATH):
             parsed_input = tuple(parse_line(line) for line in input_file)
     return tuple(x for x in parsed_input if x is not None)
 
-def solution_one(parsed_input:tuple) -> str:
+def solution_one(parsed_input:tuple[CardHand,...]) -> str:
     """ Takes the (parsed) input of the puzzle and uses it to solve for
     the first star of the day. """
-    return ""
+    ordered_cards = sorted(parsed_input)
+    total_winnings = sum(card.bid * order for order,card in enumerate(ordered_cards,start = 1))
+    print("\n".join(repr(card) for card in ordered_cards))
+    return str(total_winnings)
 
 def solution_two(parsed_input:tuple) -> str:
     """ Takes the (parsed) input of the puzzle and uses it to solve for
--- Rust/src/days/day05.rs
+++ Rust/src/days/day05.rs
@@ -1,9 +1,56 @@
 use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
+struct Range {
+    start:u64,
+    end:u64
+}
+
+struct MappingRange {
+    source:Range,
+    destination:Range
+}
+
+struct MappingRule {
+    name:String,
+    ranges:Vec<MappingRange>
+}
+
+
+
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
+    let lines = read_to_string("/input/day05_1.txt").unwrap();
+    let mut line_iter = lines.lines();
+    //Parsing, step one: Pull out the Seeds and split to numbers.
+    let seed_line = line_iter
+        .next()
+        .unwrap();
+    let seeds:Vec<u64> = seed_line.split(':')
+        .nth(1)
+        .unwrap()
+        .split(' ')
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
+    line_iter.next(); //Skip over the initial empty line.
+    let mut rules:Vec<MappingRule> = Vec::with_capacity(7);
+    let mut temp_lines:Vec<MappingRange> = Vec::new();
+    let digits = "0123456789";
+
+    for line in line_iter {
+        let first = line.chars().next().unwrap();
+        if digits.contains(first) {
+            //Number line; make a new mapping range.
+            let new_range:Vec<u64> = line.trim().split(" ").map(|x|x.parse::<u64>().unwrap()).collect();
+            
+        } else if line.trim().len() == 0 {
+
+        } else {
+
+        }
+    }
+
     // Your solution here...
     let sol1: u64 = 0;
     let sol2: u64 = 0;
