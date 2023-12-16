9331cc5b756bad95cb260cd622eda40aeb256120
Vera van den Heuvel <ray10k@xmsnet.nl>
2023-12-16 20:40:21 +0100

    day 11 done (py)

--- Python/src/days/day11.py
+++ Python/src/days/day11.py
@@ -21,15 +21,87 @@ def parse_input(file_path = INPUT_PATH):
             parsed_input = tuple(parse_line(line) for line in input_file)
     return tuple(x for x in parsed_input if x is not None)
 
-def solution_one(parsed_input:tuple) -> str:
+def solution_one(parsed_input:tuple[str,...]) -> str:
     """ Takes the (parsed) input of the puzzle and uses it to solve for
     the first star of the day. """
-    return ""
+    # Step 1: Find the empty rows, and empty collumns.
+    empty_collumns = [x for x in range(len(parsed_input[0]))]
+    empty_rows = []
+    galaxies = set()
+    for y,line in enumerate(parsed_input):
+        if line.count('#') > 0:
+            next_start = 0
+            while (index := line.find('#',next_start)) >= 0:
+                next_start = index + 1
+                galaxies.add((index,y))
+                try:
+                    empty_collumns.remove(index)
+                except:
+                    pass
+        else:
+            empty_rows.append(y)
+    # Step 2: adjust the galaxies.
+    shifted_galaxies = set()
+    for x,y in galaxies:
+        offset = 0
+        for coll in empty_collumns:
+            if coll < x:
+                offset += 1
+        x += offset
+        offset = 0
+        for row in empty_rows:
+            if row < y:
+                offset += 1
+        y += offset
+        shifted_galaxies.add((x,y))
+    # Step 3: calculate *all* the distances between galaxies.
+    retval = 0
+    while len(shifted_galaxies) > 0:
+        current_galaxy = shifted_galaxies.pop()
+        for other in shifted_galaxies:
+            retval += (abs(current_galaxy[0]-other[0])+abs(current_galaxy[1]-other[1]))
+    return str(retval)
 
 def solution_two(parsed_input:tuple) -> str:
     """ Takes the (parsed) input of the puzzle and uses it to solve for
     the second star of the day. """
-    return ""
+        # Step 1: Find the empty rows, and empty collumns.
+    empty_collumns = [x for x in range(len(parsed_input[0]))]
+    empty_rows = []
+    galaxies = set()
+    for y,line in enumerate(parsed_input):
+        if line.count('#') > 0:
+            next_start = 0
+            while (index := line.find('#',next_start)) >= 0:
+                next_start = index + 1
+                galaxies.add((index,y))
+                try:
+                    empty_collumns.remove(index)
+                except:
+                    pass
+        else:
+            empty_rows.append(y)
+    # Step 2: adjust the galaxies.
+    shifted_galaxies = set()
+    for x,y in galaxies:
+        offset = 0
+        for coll in empty_collumns:
+            if coll < x:
+                offset += 999_999
+        x += offset
+        offset = 0
+        for row in empty_rows:
+            if row < y:
+                offset += 999_999
+        y += offset
+        shifted_galaxies.add((x,y))
+    # Step 3: calculate *all* the distances between galaxies.
+    retval = 0
+    while len(shifted_galaxies) > 0:
+        current_galaxy = shifted_galaxies.pop()
+        for other in shifted_galaxies:
+            retval += (abs(current_galaxy[0]-other[0])+abs(current_galaxy[1]-other[1]))
+    return str(retval)
 
 def solve_day() -> tuple[float,float,float]:
     times = [0,0,0,0]
